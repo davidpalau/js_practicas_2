d2ed649e25b89b12d04086f5086025ac14abca81
davidpalau <palau.david@hotmail.com>
2022-11-11 20:26:27 +0100

    ajfdklñ

--- js/acti_9_a.js
+++ js/acti_9_a.js
@@ -1,78 +1,30 @@
-/**
- * ACTIVITY 9 – Group students 
-- Create  a  web  application  that  reads  the  names  of  the  so-called  students  (anything  entered 
-will be valid) 
-- The introduction of students ends when we indicate the word end. 
-- Names  cannot  be  repeated,  if  it  is  repeated  it  is  indicated  that  the  name  is  repeated  and 
-another one is requested. 
-- The application will group users 3 by 3 into random groups. 
-- If the number of students is not a multiple of three, the remaining students move one by one 
-randomly in the groups that we indicate. 
-- Example of result: 
-o Grupo 1: David, Toni, Edison 
-o Group 2: Sandra, Isabel, Jaume, Carlos 
-o Group 3: Ricardo, Vicent, Joan 
-o Group 4: Raúl, Manel, Juan, Alejandro 
-− There  are  two  groups  of  4  students  as  there  are  more  students,  it  is  important  that  the  2 
-students are not placed in the same group forming a group of 5. 
-
- * 
- */
-
-window.addEventListener("load", inicio, true);
-//inicio
-function inicio() {
-    let condition = true;
-    let todosLosNombres = [];
-    let miArray = [];
-    let nombre;
-    let grupo = [];
-    let aleatoria;
-    do {
-        grupo = [3];
-        for (let j = 0; j < 3; j++) {
-            nombre = window.prompt("Introduce nombre");
-            //
 
 
-            if (nombre == "f") {
-                condition = false;
-                j = 4;
-            } else {
-
-
-                if (todosLosNombres.includes(nombre)) {
-
-                    window.alert("El nombre esta repetido.");
-                } else {
-                    grupo[j] = nombre;
-                    todosLosNombres.push(nombre);
-                }
-
-
-            }
-        }
-        if (nombre == "fin") {
-
-        } else {
-            miArray.push(grupo);
-
-        }
-
-
-    } while (condition);
-
-    console.log(miArray);
-    console.log(grupo);
-    if (grupo.length == 1) {
-        aleatoria = parseInt(Math.random(miArray.length));
-        miArray[0].push(grupo[0]);
-    }
-    aleatoria = parseInt(Math.random(miArray.length));
-    miArray[0].push(grupo[0]);
-
-    console.log(todosLosNombres);
-
-
+let datos = {
+    1: "Óptimo",
+        2: "Notable",
+     3: "Notable",
+    4: "Excelente",
+   5: "Improbable",
+     6: "Excelente",
+    7: "Notable"
 }
-
+//Obtenemos las llaves actuales. 
+let respuesta = Object.keys(datos)
+    .reduce((acumulador, keyAnterior) => {
+        //Obtenemos la nueva key deseada. Que es el valor. 
+        let nuevaKey = datos[keyAnterior]
+      //Si el acumulador no contiene la nueva clave, la creamos y
+      // la inicializamos con un arreglo vacio. 
+      if (!acumulador.hasOwnProperty(nuevaKey))
+        acumulador[nuevaKey] = []
+      //Agregamos la key anterior al arreglo. 
+      acumulador[nuevaKey].push(keyAnterior)
+      //Retornamos los cambios al acumulador para seguir con el
+      //ciclo. 
+      return acumulador
+    //Este segundo parametro {} es para inicializar el acumulador. 
+    //En este caso un objeto. 
+    }, {})
+  
+  console.log(respuesta)
